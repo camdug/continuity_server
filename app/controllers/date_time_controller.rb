ab5187e4c4c1b36d16818e65b9d9381f16697cda
Alastair Smith <github@alastairsmith.me.uk>
2011-03-19 17:18:55 +0000

    Fix up time zone bug that was causing the offset to be displayed in seconds and not hours.

--- app/controllers/date_time_controller.rb
+++ app/controllers/date_time_controller.rb
@@ -3,12 +3,13 @@ class DateTimeController < ApplicationController
   respond_to :html, :json
 
   def get
-    time_zone = params[:time_zone]
+    time_zone = params[:time_zone]    
+    seconds_in_a_day = 60*60*24;
     
     if (time_zone.nil?)
       @time = DateTime.now
     else
-      @time = DateTime.now.change(:offset => Time.zone_offset(time_zone))
+      @time = DateTime.now.change(:offset => Rational(Time.zone_offset(time_zone), seconds_in_a_day))
     end
     
     respond_with(@time)
